148d3bfbf80ef74a2d51a0e907f0dad807016860
nicholaslee119 <nicholaslee119@gmail.com>
2017-09-21 13:25:24 +0900

    add test of buildJSandCSS

---
+++ test/buildJSandCSS.test.js
@@ -0,0 +1,48 @@
+import klawSync from 'klaw-sync';
+import path from 'path';
+
+import buildJSandCSS from '../lib/buildJSandCSS';
+import buildOptionNormal from '../test/buildOption.normal';
+
+describe('test buildJSandCSS', function(){
+
+  it('normal', function(){
+    const normalComponents =
+      [
+        {
+          "root": "",
+          "dir": "include",
+          "base": "componentA.tpl",
+          "ext": ".tpl",
+          "name": "componentA"
+        },
+        {
+          "root": "",
+          "dir": "include",
+          "base": "componentD.tpl",
+          "ext": ".tpl",
+          "name": "componentD"
+        },
+        {
+          "root": "",
+          "dir": "",
+          "base": "componentB.tpl",
+          "ext": ".tpl",
+          "name": "componentB"
+        },
+        {
+          "root": "/",
+          "dir": "",
+          "base": "pageC.tpl",
+          "ext": ".tpl",
+          "name": "pageC"
+        }
+      ];
+    buildJSandCSS(normalComponents, buildOptionNormal);
+    const builtCSS = klawSync(path.join(__dirname, './assetsCoreTest/css'), {nodir: true});
+    const builtJS = klawSync(path.join(__dirname, './assetsCoreTest/js'), {nodir: true});
+    expect(builtJS).toHaveLength(1);
+    expect(builtCSS).toHaveLength(1);
+  })
+
+})
