a51aa42e20be4316e0f81dcbf4ec99e216b286dc
nicholaslee119 <nicholaslee119@gmail.com>
2017-09-22 14:11:05 +0900

    adjust the syntax which webpack loader cannot use

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import loaderUtils from 'loader-utils';
-import core from './lib/core';
+const loaderUtils = require('loader-utils');
+const core = require('./lib/core');
 
 module.exports = function (source) {
   const options = loaderUtils.getOptions(this);
   const extractor = options.extractor;
+  const injector = options.injector;
   const ext = options.ext;
   const srcPath = options.srcPath;
   const builtJSPath = options.builtJSPath;
   const builtCSSPath = options.builtCSSPath;
   const builtTemplatePath = options.builtTemplatePath;
   const selfPath = this.resourcePath;
-  core(source, extractor, ext, srcPath, builtJSPath, builtCSSPath, builtTemplatePath, selfPath);
+  core(source, extractor, injector, ext, srcPath, builtJSPath, builtCSSPath, builtTemplatePath, selfPath);
   return '';
 }
--- test/injector.normal.js
+++ test/injector.normal.js
@@ -1,15 +1,9 @@
-
-function createAssetsTags(assetsURL) {
-  const tags = {};
-  tags.js = `<script src="${assetsURL.js}"/>`;
-  tags.css = `<link rel="stylesheet" href="${assetsURL.css}" type="text/css">`;
-  return tags;
-}
-
 export default function injectAssets(template, assetsURL) {
-  const assetTags = createAssetsTags(assetsURL);
+  const assetTags = {};
+  assetTags.js = `<script src="${assetsURL.js}"/>`;
+  assetTags.css = `<link rel="stylesheet" href="${assetsURL.css}" type="text/css">`;
   const inserted = template
-  .replace('{* javaScript insertion *}', assetTags.js ? assetTags.js : '')
-  .replace('{* css insertion *}', assetTags.css ? assetTags.css : '');
+    .replace('{* javaScript insertion *}', assetTags.js ? assetTags.js : '')
+    .replace('{* css insertion *}', assetTags.css ? assetTags.css : '');
   return inserted;
 }
