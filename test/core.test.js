1488334854173dc9c383569f053ed472d433f10f
nicholaslee119 <nicholaslee119@gmail.com>
2017-09-23 18:02:04 +0900

    add test to validate path

--- test/core.test.js
+++ test/core.test.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import core from '../lib/core';
+import fsx from 'fs-extra';
 
 import extractor from './util/extractor.normal';
 import injector from './util/injector.normal';
 
+function clearBuild () {
+  fsx.removeSync(path.join(__dirname, './assetsCoreTest'));
+}
+
 describe('core test', function() {
   it('smarty template test', function(){
+    clearBuild();
     const pageTpl = fs.readFileSync(path.join(__dirname, './fixture/pageC/pageC.tpl'), 'utf8');
     core(pageTpl, extractor, injector, '.tpl',
       path.join(__dirname, './fixture'),
@@ -15,6 +21,22 @@ describe('core test', function() {
       path.join(__dirname, './assetsCoreTest/templates'),
       path.join(__dirname, './fixture/pageC/pageC.tpl')
     )
-  })
+  });
+
+  it('unvalidate path should not be passed', function(){
+    clearBuild();
+    const pageTpl = fs.readFileSync(path.join(__dirname, './fixture/pageC/pageC.tpl'), 'utf8');
+    core(pageTpl, extractor, injector, '.tpl',
+      path.join(__dirname, './!!!!'),
+      path.join(__dirname, '..../assetsCoreTest/js'),
+      [1,24,22],
+      path.join(__dirname, './assetsCoreTest/templates'),
+      ''
+    );
+    const dirs = fsx.pathExistsSync(path.join(__dirname, './assetsCoreTest'));
+    expect(dirs).toBeFalsy();
+  });
+
+
 })
 
