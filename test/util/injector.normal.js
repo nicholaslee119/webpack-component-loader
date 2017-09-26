44e3f7888bb624acd3a369988583f6b7dbeb5bf7
nicholaslee119 <nicholaslee119@gmail.com>
2017-09-26 21:38:49 +0900

    add the test for insertJSandCSS

--- test/util/injector.normal.js
+++ test/util/injector.normal.js
@@ -1,7 +1,7 @@
 export default function injectAssets(template, assetsURL) {
   const assetTags = {};
-  assetTags.js = `<script src="${assetsURL.js}"/>`;
-  assetTags.css = `<link rel="stylesheet" href="${assetsURL.css}" type="text/css">`;
+  if(assetsURL.js) assetTags.js = `<script src="${assetsURL.js}"/>`;
+  if(assetsURL.css) assetTags.css = `<link rel="stylesheet" href="${assetsURL.css}" type="text/css">`;
   const inserted = template
     .replace('{* javaScript insertion *}', assetTags.js ? assetTags.js : '')
     .replace('{* css insertion *}', assetTags.css ? assetTags.css : '');
