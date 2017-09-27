981480a6f3a198a6314af9a9ea2c3399b4ea6d64
nicholaslee119 <nicholaslee119@gmail.com>
2017-09-27 13:47:44 +0900

    improve parse

--- test/util/extractor.normal.js
+++ test/util/extractor.normal.js
@@ -1,9 +1,14 @@
-export default function templateExtractor (source) {
-  var included = source.match(/\'.*\'/g);
+export default function templateExtractor (source){
+  var included = source.match(/\{include .*\}/g);
+  var fileREG = /file\s?=\s?["'].*?["']/;
+  var quotationREG = /["'].*?["']/;
   if(!included) return [];
   var res = [];
   included.forEach(function (element) {
-    res.push(element.slice(1,element.length-1));
-  })
+    let includeFile = fileREG.exec(element)[0];
+    let file = quotationREG.exec(includeFile)[0];
+    res.push(file.slice(1,file.length-1));
+  });
   return res;
 }
+
--- test/util/injector.normal.js
+++ test/util/injector.normal.js
@@ -3,7 +3,7 @@ export default function injectAssets(template, assetsURL) {
   if(assetsURL.js) assetTags.js = `<script src="${assetsURL.js}"/>`;
   if(assetsURL.css) assetTags.css = `<link rel="stylesheet" href="${assetsURL.css}" type="text/css">`;
   const inserted = template
-    .replace('{* javaScript insertion *}', assetTags.js ? assetTags.js : '')
-    .replace('{* css insertion *}', assetTags.css ? assetTags.css : '');
+  .replace('{* javaScript insertion *}', assetTags.js ? assetTags.js : '')
+  .replace('{* css insertion *}', assetTags.css ? assetTags.css : '');
   return inserted;
 }
