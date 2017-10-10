8438981da138ecfaf5530b8f08dd58d7bb4f2d57
nicholaslee119 <nicholaslee119@gmail.com>
2017-10-10 21:27:10 +0900

    update config

--- test/fixture/webpack.config.js
+++ test/fixture/webpack.config.js
@@ -1,32 +1,63 @@
-import path from 'path';
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
 
 import {extractor} from 'webpack-component-loader-smarty-parser';
 
 module.exports = {
-  entry: './test/fixture/entry.js',
+  entry: {
+    entryA: './test/fixture/entryA.js',
+    entryB: './test/fixture/entryB.js'
+  },
+  output: {
+    path: path.resolve(__dirname, "../assets/"),
+    filename     : 'js/[name].js',
+    chunkFilename: 'js/[name].chunk.js',
+  },
   resolveLoader: {
     alias: {
       'webpack-component-loader': path.join(__dirname, '../../index.js'),
     },
-  },
-  output: {
-    path: path.resolve(__dirname, '..', 'assets', 'js'),
-    filename: "bundle.js",
-    publicPath: "/assets/",
   },
   module: {
     rules: [
       {
         test   : /\.tpl?$/,
         exclude: /(node_modules)/,
-        loader : 'webpack-component-loader',
-        query  : {
-          extractor : extractor,
-          ext: '.tpl',
-          srcPath : path.resolve(__dirname, '.'),
-          builtTemplatePath : path.resolve(__dirname, '../assets/templates'),
-        },
+        use: [
+          {
+            loader: 'webpack-component-loader',
+            options: {
+              isCodeSplit: false,
+              extractor : extractor,
+              ext: '.tpl',
+              srcPath : path.join(__dirname, '.'),
+              builtJSPath : path.join(__dirname, '../assets/js'),
+              builtCSSPath : path.join(__dirname, '../assets/css'),
+              builtTemplatePath : path.join(__dirname, '../assets/templates'),
+            },
+          },
+        ],
       },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        enforce: "post",
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
     ],
   },
+  plugins: [
+    new ExtractTextPlugin({
+      filename:  "css/[name].css",
+      // allChunks: true
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "commons",
+      filename: "js/commons.js",
+    })
+  ]
 }
